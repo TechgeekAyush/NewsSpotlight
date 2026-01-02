63e37c0a19fe8ec5a1adbef82a0b3c82d9cdc5f6
Ayush Udayakumar Nair <ayushun2003@gmail.com>
2026-01-02 10:26:26 +0530

    Changes for deployment

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react'
 import NewsItem from './NewsItem'
 
 const News = (props) => {
+  const apiKey = import.meta.env.VITE_REACT_NEWS_API_KEY;
   document.title = `${(props.category).charAt(0).toUpperCase() + (props.category).slice(1)} - NewsSpotlight`
   const [articles, setArticles] = useState([]);
   const updateNews = async () => {
     props.setProgress(10)
-    // In News.jsx, inside updateNews function
-    const url = `/api/news?category=${props.category}`;
+    const url = `https://gnews.io/api/v4/top-headlines?lang=en&country=in&category=${props.category}&apikey=${apiKey}`
     let data = await fetch(url);
     // props.setProgress(30);
     let parsedData = await data.json()
@@ -21,7 +21,7 @@ const News = (props) => {
   return (
     <>
       <p className='text-center text-2xl font-extrabold p-3'>NewsSpotlight - Top {(props.category).charAt(0).toUpperCase() + (props.category).slice(1)} Headlines</p>
-      <div className="flex flex-wrap justify-center mx-auto gap-10">
+      <div className="flex flex-wrap justify-center gap-10">
         {articles.map((element) => {
           return <NewsItem key={element.url} title={element.title} description={element.description} image={element.image} url={element.url} sourcename={element.source.name} publishedAt={element.publishedAt} />
         })}
